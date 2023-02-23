3d5f51438a6c9649a8581404d1e8f0698b235f19
Virginie Allflatt <vallflatt@gmail.com>
2023-02-23 20:41:39 +0100

    fixed list route

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+List.destroy_all
+Movie.destroy_all
+
+Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
+Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
+Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
+Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+Movie.create(title: "La petite sirène", overview: "Arielle veut devenir humaine.", poster_url: "https://www.disneyphile.fr/wp-content/uploads/2022/10/la-petite-sirene-teaser-affiche-960x540.jpg.webp", rating: 7.0)
+
+List.create(name: "super films que j'aime")
+List.create(name: "films d'horreur")
+List.create(name: "classiques cinéma français")
+List.create(name: "noir et blanc")
+List.create(name: "bio pics")
